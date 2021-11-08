9e599944a4e5109c23f0b4f52021006f6db401be
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2021-11-08 09:54:35 +0100

    Make clean-failed-build more likely to be helpful

--- clean-failed-build.sh
+++ clean-failed-build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 rm -f *.raw
-umount target/dev
-umount target/proc
-umount target/sys
-umount target
-rmdir target
+if [ -d target/dev ]; then umount target/dev; fi
+if [ -d target/proc ]; then umount target/proc; fi
+if [ -d target/sys ]; then umount target/sys; fi
+if [ -d target ]; then umount target || true; rmdir target; fi
 losetup -d /dev/loop0
